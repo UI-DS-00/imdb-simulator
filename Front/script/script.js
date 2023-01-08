e5449cd7802f8ab5bd128e93023bb83f6ef47894
BehnoodOboodiyyat <behnoodoboodiyyat@gmail.com>
2023-01-08 15:36:35 +0330

    artist's page added :)

--- Front/script/script.js
+++ Front/script/script.js
@@ -31,3 +31,27 @@ ball.addEventListener("click",()=>{
   })
   ball.classList.toggle("active")
 })
+
+
+
+// const apiBassUrl = "https://api.themoviedb.org/3";
+// const apikey = "467debcf216a77cd7a1066718148bc9d";
+// const imageBaseUrl = 'https://image.tmdb.org/t/p/w300';
+
+// async function fetchMoviesNowPlaying() {
+//     const response = await fetch(${apiBaseUrl}/movie/now_playing?api_key=${apiKey});
+//     const jsonResponse = await response.json();
+//     const movies = jsonResponse.results;
+//     console.log(movies);
+// }
+// fetchMoviesNowPlaying();
+
+// const moviesGrid = document.getElementById("movie-grid");
+
+// function displayMovies(movies) {
+//     moviesGrid.innerHTML = movies.map(movie => 
+//         `<img src="${imageBaseUrl}${movie.poster_path}"/>
+//         <p>⭐️${movie.vote_average}</p>
+//         <h1>${movie.title}</h1>`
+//     ).join("");
+// }
