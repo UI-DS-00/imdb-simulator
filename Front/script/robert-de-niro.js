a1f181c191a66728db9b17b799501c052820fbc4
BehnoodOboodiyyat <behnoodoboodiyyat@gmail.com>
2023-01-08 16:28:28 +0330

    Robert Deniro page added

---
+++ Front/script/robert-de-niro.js
@@ -0,0 +1,10 @@
+'use strict';
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.featured-movie,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.movie-list-item-title,.movie-list-item-desc");
+
+ball.addEventListener("click",()=>{
+  items.forEach(item=>{
+    item.classList.toggle("active")
+  })
+  ball.classList.toggle("active")
+})
